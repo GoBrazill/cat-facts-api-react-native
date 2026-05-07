d7135dbc558e81028e1b31835e7da190be4828e1
GoBrazill <vinicius.daraujos26@gmail.com>
2026-05-07 11:08:45 -0300

    feat: styled elements in app js

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -5,7 +5,28 @@ export const GlobalStyles = StyleSheet.create({
     container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+    gap: 16,
     backgroundColor: '#F0E2CE',
   },
+    title: {
+      fontSize: 48,
+      fontWeight: 'bold',
+      color: 'hsla(35, 53%, 20%, 1.00)'
+    },
+    fact: {
+      textAlign: 'center',
+      width: '60vw',
+      fontSize: 36,
+      fontWeight: '500',
+      color: 'crimson',
+    },
+    button: {
+      padding: 16,
+      backgroundColor: 'crimson', 
+      borderRadius: 10
+    },
+    textButton: {
+      color: 'hsla(35, 53%, 90%, 1.00)'
+    }
 })
