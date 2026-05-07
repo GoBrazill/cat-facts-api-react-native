cabdc18e4b7501e6893cff5f4372469d124d06fc
GoBrazill <vinicius.daraujos26@gmail.com>
2026-05-07 11:08:24 -0300

    feat: add function to generate fact

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StyleSheet,Text, View } from 'react-native';
+import { Text, View, Button, TouchableOpacity } from 'react-native';
 
 import { GlobalStyles } from './src/styles/GlobalStyles';
 import api from './src/services/api';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [fato, setFato] = useState('Clique no botão para gerar um fato!')
+  async function carregarFatos() {
+    try {
+      const res = await api.get('fact')
+      setFato(res.data.fact)
+    } catch (err) {
+      setFato('Erro ao carregar fato')
+    }
+  }
+
   return (
     <View style={GlobalStyles.container}>
-      <Text> test </Text>
+      <Text style={GlobalStyles.title}> Fatos sobre Gatos</Text>
+      <Text style={GlobalStyles.fact}> {fato} </Text>
+      <TouchableOpacity title='Carregar Fato' onPress={carregarFatos} style={GlobalStyles.button} >
+
+      <Text style={GlobalStyles.textButton}> Carregar Fato </Text>
+      </TouchableOpacity>
+          
     </View>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://catfact.ninja/fact'
+    baseURL: 'https://catfact.ninja/'
 })
 
 export default api
