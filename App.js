03c8649d7e8bd3b6a685d2e0be923b0983c9925b
GoBrazill <vinicius.daraujos26@gmail.com>
2026-05-06 22:52:13 -0300

    chore: add fav icon and remove unused images

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Text, View } from 'react-native';
+
+import { GlobalStyles } from './src/styles/GlobalStyles';
+import api from './src/services/api';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={GlobalStyles.container}>
+      <Text> test </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
